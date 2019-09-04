abd761e3aa17ab629973a91c2216931155be9d0d
aayush <aayush9152@gmail.com>
2019-09-04 19:39:25 +0530

    update

---
+++ backend/route/mainRoutes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const mainController = require('../controller/mainController');
+const signUpController = require('../controller/signUpController');
+const loginController = require('../controller/loginController');
